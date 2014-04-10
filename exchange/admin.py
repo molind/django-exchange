94d294d55dc9c31e269c6ba550d127474d98d5ed
Evgen Bodunov <molind@gmail.com>
2014-04-10 15:05:35 +0300

    more fixes

--- exchange/admin.py
+++ exchange/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models import Currency, ExchangeRate
+from exchange.models import Currency, ExchangeRate
 
 admin.site.register(Currency)
 admin.site.register(ExchangeRate)
